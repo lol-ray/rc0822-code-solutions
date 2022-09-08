9ffa1b3ff63d376b739cbacc790e6598d9c3e0d9
Ray Le <keri.my.le@gmail.com>
2022-09-08 17:53:38 +0000

    Revised.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -5,5 +5,7 @@ function clickTask(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
   console.log('closest .task-list-item', event.target.closest('.task-list-item'));
-  event.target.closest('.task-list-item').remove();
+  if (event.target && event.target.nodeName === 'BUTTON') {
+    event.target.closest('.task-list-item').remove();
+  }
 }
