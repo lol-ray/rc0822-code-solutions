1b4ab3e27332a47dd1cfae6f1c9f16ed7acee6ec
Ray Le <keri.my.le@gmail.com>
2022-09-08 17:46:06 +0000

    Completed dom-event-delegation exercise.

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,9 @@
+var $taskList = document.querySelector('.task-list');
+$taskList.addEventListener('click', clickTask);
+
+function clickTask(event) {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+  console.log('closest .task-list-item', event.target.closest('.task-list-item'));
+  event.target.closest('.task-list-item').remove();
+}
