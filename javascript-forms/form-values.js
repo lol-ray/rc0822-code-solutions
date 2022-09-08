84fb605f8970182ea66d208e7fc6e05f0d3bd5a6
Ray Le <keri.my.le@gmail.com>
2022-09-08 03:40:02 +0000

    Completed javascript-forms exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,13 @@
+var contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', contactSubmit);
+
+function contactSubmit(event) {
+  event.preventDefault();
+  const messageData = {
+    name: contactForm.elements.name.value,
+    email: contactForm.elements.email.value,
+    message: contactForm.elements.message.value
+  };
+  console.log('messageData: ', messageData);
+}
