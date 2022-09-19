169023daab87c5903d44ae429b0f32717f5fb4a3
Ray Le <keri.my.le@gmail.com>
2022-09-19 18:23:57 +0000

    Completed javascript-this exercise.

---
+++ javascript-this/student.js
@@ -0,0 +1,13 @@
+/* exported student */
+
+var student = {
+  firstName: 'Ray',
+  lastName: 'Le',
+  subject: 'JavaScript',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+  }
+};
