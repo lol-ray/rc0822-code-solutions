062643e597803610c0186cc6fb26db7b053cedac
Ray Le <keri.my.le@gmail.com>
2022-11-09 20:01:52 +0000

    Completed react-accordion-component exercise.

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openId: null,
+      firstClick: true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id) {
+    if (this.state.firstClick) {
+      this.setState({
+        openId: id,
+        firstClick: false
+      });
+    } else {
+      this.setState({
+        openId: false,
+        firstClick: true
+      });
+    }
+  }
+
+  render() {
+    const accordion = this.props.accordion;
+    const content = accordion.map(accordion =>
+      <div className="container" key={accordion.id}>
+        <h1 onClick={() => this.handleClick(accordion.id)}>
+          {accordion.topic}
+        </h1>
+        {
+          this.state.openId === accordion.id
+            ? (
+              <p className='active'>
+                {accordion.description}
+              </p>
+              )
+            : (
+              <p className=''>
+                {accordion.description}
+              </p>
+              )
+        }
+      </div>
+    );
+    return (
+      <div className="accordion">
+        {content}
+      </div>
+    );
+  }
+}
